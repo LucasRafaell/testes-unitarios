0ec37e6da9c991a1fb46d5b4d42109d1e474ec96
Lucas Rafael <lucasrafaelinfo@gmail.com>
2023-08-09 21:48:16 -0300

    Testes em paralelo Parte 2

--- TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -1,30 +1,34 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.BlockJUnit4ClassRunner;
-import org.junit.runners.JUnit4;
 
 import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
-import br.ce.wcaquino.runners.ParallelRunner;
 
-@RunWith(ParallelRunner.class)
 public class CalculadoraTest {
 
+	public static StringBuffer ordem = new StringBuffer();
+	
 	private Calculadora calc;
 	
 	@Before
 	public void setup() {
 		calc = new Calculadora();
 		System.out.println("iniciando...");
+		ordem.append("1");
 	}
 	
 	@After
 	public void tearDown() {
 		System.out.println("finalizando...");
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println(ordem.toString());
 	}
 	
 	@Test
--- TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculoValorLocacaoTest.java
+++ TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/CalculoValorLocacaoTest.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -49,6 +51,18 @@ public class CalculoValorLocacaoTest {
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+		System.out.println("Iniciando 3...");
+		CalculadoraTest.ordem.append(3);
+	}
+	
+	@After
+	public void tearDown() {
+		System.out.println("finalizando 3...");
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println(CalculadoraTest.ordem.toString());
 	}
 	
 	private static Filme filme1 = FilmeBuilder.umFilme().agora();
@@ -72,10 +86,12 @@ public class CalculoValorLocacaoTest {
 	}
 	
 	@Test
-	public void deveCalcularValorLocacaoConsiderandoDescontos() throws FilmeSemEstoqueException, LocadoraException {
+	public void deveCalcularValorLocacaoConsiderandoDescontos() throws FilmeSemEstoqueException, LocadoraException, InterruptedException {
 		//cenario
 		Usuario usuario = new Usuario("Usuario 1");
-				
+			
+		Thread.sleep(5000);
+		
 		//acao
 		Locacao resultado = service.alugarFilme(usuario, filmes);
 		
